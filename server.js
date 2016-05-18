8c3271efd654a9667b2b482fae24d9d0f8792d86
Natalia Stuchynska <natalia.stuchynska@synapse.com>
2016-05-18 11:40:30 +0300

    realises checking for the existence user on server side

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ var urlencodedParser = bodyParser.urlencoded({
 });
 
 var mkdirp = require('mkdirp');
+var guestMode = false;
 
 app.use(busboy());
-
 app.use(cookieParser());
 app.use(session({
     cookieName: 'session',
@@ -40,23 +40,26 @@ app.post('/login', urlencodedParser, function(req, res) {
     } else {
         res.redirect('back');
     }
-    res.end(JSON.stringify(user));
+    res.end();
 });
 
-app.post('/changeAvatarka', function (req, res) {
+app.post('/changeAvatarka', function(req, res) {
+    if (guestMode) {
+        res.end();
+    }
     console.log(loggedUser.avatar);
     var userSes = req.session.user;
     var path;
     var ftream;
     var pathToAvatar;
-    req.busboy.on('file', function (fieldname, file, filename) {
+    req.busboy.on('file', function(fieldname, file, filename) {
         path = __dirname + '/users_images/' + userSes.login + "/" + filename;
         ftream = fs.createWriteStream(path);
         file.pipe(ftream);
         pathToAvatar = users[loggedUser.login].avatar;
-        pathToAvatar = 'users_images/' + userSes.login +'/' + filename;      
-        loggedUser.avatar = 'users_images/' + userSes.login +'/' + filename;    
-        fs.writeFile('./users.json', JSON.stringify(users), function(err) {
+        pathToAvatar = 'users_images/' + userSes.login + '/' + filename;
+        loggedUser.avatar = 'users_images/' + userSes.login + '/' + filename;
+        fs.writeFile('./users.json', JSON.stringify(users, null, '\t'), function(err) {
             if (err) {
                 console.log(err);
             } else {
@@ -65,13 +68,15 @@ app.post('/changeAvatarka', function (req, res) {
         });
         ftream.on('close', function() {
             res.redirect('/userpage.html');
-            // console.log(res);
         });
     });
     req.pipe(req.busboy);
 });
 
 app.post('/fileupload', function(req, res) {
+    if (guestMode) {
+        res.end();
+    }
     var fields = {};
     var pathDirect;
     var path = './users_images/' + loggedUser.login;
@@ -113,30 +118,10 @@ app.post('/fileupload', function(req, res) {
     req.pipe(req.busboy);
 });
 
-function writeToJSON(fileName, fields) {
-    var images = require('.' + loggedUser.images);
-    var newPhoto = {};
-    newPhoto.id = String(getRandomPhotoID(0, 999));
-    newPhoto.src = '/users_images/' + loggedUser.login + "/" + fileName;
-    newPhoto.descr = fields.fotoDescribe;
-    newPhoto.category = fields.categoryName;
-    var checked = false;
-    if (fields.privateFoto) {
-        checked = true;
-    }
-    newPhoto.private = checked;
-    console.log(newPhoto);
-    images.push(newPhoto);
-    fs.writeFile('.' + loggedUser.images, JSON.stringify(images), function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("File saved. New photo was add.");
-        }
-    });
-}
-
 app.post('/remove_photo', urlencodedParser, function(req, res) {
+    if (guestMode) {
+        res.end();
+    }
     var remPhotoID = req.body.id;
     var images = require('.' + loggedUser.images);
     for (var i = 0; i < images.length; i++) {
@@ -146,7 +131,7 @@ app.post('/remove_photo', urlencodedParser, function(req, res) {
             break;
         }
     }
-    fs.writeFile('.' + loggedUser.images, JSON.stringify(images), function(err) {
+    fs.writeFile('.' + loggedUser.images, JSON.stringify(images, null, '\t'), function(err) {
         if (err) {
             console.log(err);
         } else {
@@ -158,15 +143,26 @@ app.post('/remove_photo', urlencodedParser, function(req, res) {
 
 app.get('/user_page', function(req, res) {
     if (!req.session.user) {
-        res.send({error: 'not logged in' });
+        res.send({error: 'not logged in'});
         return;
     }
-    res.send(loggedUser);
+    if (req.query.user) {
+
+        if ( (findUser(req.query.user) === false) || (req.query.user === loggedUser.login)){
+            res.send([loggedUser, true]);
+        } else {
+            guestMode = true;
+            res.send([findUser(req.query.user), false]);
+        }
+
+    } else {
+        res.send([loggedUser, true]);
+    }
+    res.end();
 });
 
 app.get('/logout', function(req, res) {
     req.session.reset();
-    // delete req.session.user;
     res.redirect('/index.html');
 });
 
@@ -183,6 +179,39 @@ function isUser(user, users) {
     return user.password === users[user.login].password;
 };
 
+function findUser(login) {
+    // return users[login] || false;
+    if(users[login]) {
+        var guest = JSON.parse(JSON.stringify(users[login]));
+        delete guest.password;
+        return guest;
+    }
+    return false;
+}
+
 function getRandomPhotoID(min, max) {
     return Math.round(Math.random() * (max - min) + min);
+}
+
+function writeToJSON(fileName, fields) {
+    var images = require('.' + loggedUser.images);
+    var newPhoto = {};
+    newPhoto.id = String(getRandomPhotoID(0, 999));
+    newPhoto.src = '/users_images/' + loggedUser.login + "/" + fileName;
+    newPhoto.descr = fields.fotoDescribe;
+    newPhoto.category = fields.categoryName;
+    var checked = false;
+    if (fields.privateFoto) {
+        checked = true;
+    }
+    newPhoto.private = checked;
+    console.log(newPhoto);
+    images.push(newPhoto);
+    fs.writeFile('.' + loggedUser.images, JSON.stringify(images, null, '\t'), function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("File saved. New photo was add.");
+        }
+    });
 }
--- userPageLoad.js
+++ userPageLoad.js
@@ -1,74 +1,78 @@
-function userPage(){
+function userPage() {
+
     var guest = false;
-    var url2 = window.location.search;
-    console.log(url2);
-    if(url2) {
+    var url2 = window.location.search || '';
+    if (url2) {
         guest = true;
         $('.btn-login').hide();
         $('#avatarka').remove();
-    };
-    console.log(guest);
-$.get('/user_page', function(res) {
-    if (res.error || !res) {
-        window.location.href = '/index.html';
-        return;
     }
-    $('.username').find('strong').text(res.name);
-    $('.status').text(res.status);
-    $('<img/>', {src: res.avatar, class: 'img-responsive img-circle'}).appendTo('.avatar'); 
-    $.get(res.images, logImages);
+
+    $.get('/user_page' + url2, function(response) {
+        if (response.error || !response) {
+            window.location.href = '/index.html';
+            return;
+        }
+        var res = response[0];
+        if(response[1]) { guest = false; }
+        console.log(res);
+        if (res.nouser) {
+            $('.username').find('strong').text(res.nouser);
+        } else {
+            $('.username').find('strong').text(res.name);
+            $('.status').text(res.status);
+            $('<img/>', {src: res.avatar, class: 'img-responsive img-circle'}).appendTo('.avatar'); 
+            $.get(res.images, logImages);
+        }
     });
 
-// $.post('/changeAvatarka', function(res) {
-//     $('<img/>', {src: res.avatar, class: 'img-responsive img-circle'}).appendTo('.avatar');
-// });
-
-function logImages(res) {
-    var galery = $('.image-list');
-    var link;
-    for (var i = 0; i < res.length; i++) {
-        link = $('<a>', {class: "thumbnail fancybox", rel: "group", href: res[i].src}).appendTo(galery);
-        if(guest == false){    
-        link.append('<span class="glyphicon glyphicon-remove remove-photo" aria-hidden="true" value="' + res[i].id + '"></span>');
+    function logImages(res) {
+        var galery = $('.image-list');
+        var link;
+        for (var i = 0; i < res.length; i++) {
+            link = $('<a>', {class: "thumbnail fancybox", rel: "group", href: res[i].src}).appendTo(galery);
+            if (guest == false) {
+                link.append('<span class="glyphicon glyphicon-remove remove-photo" aria-hidden="true" value="' + res[i].id + '"></span>');
+            }
+            link.append('<img src="' + res[i].src + '" class="img-responsive openmodal">');
+            link.append('<div class="caption">' + res[i].descr + '</div>');
         }
-        link.append('<img src="' + res[i].src + '" class="img-responsive openmodal">');
-        // <a class="fancybox" rel="group" href="users_images/sasha/1.jpg"><img src="users_images/sasha/1.jpg" alt="" /></a>
-        link.append('<div class="caption">' + res[i].descr + '</div>');
+    }
+
+    $('.image-list').on('click', '.remove-photo', function(event) {
+        event.preventDefault();
+        if (guest) {
+            return;
+        }
+        var image = $(this).parent().removeClass('fancybox');
+        var imageID = {id: $(this).attr('value')};
+        console.log(imageID);
+        $.post('/remove_photo', imageID, function(res) {
+            if (res.status) {
+                image.remove();
+            }
+        });
+    });
+
+    if (guest == false) {
+        $(".avatar").on("mouseover", ".img-circle", function() {
+            $("div#alex").show("slow");
+        }).on("mouseout", ".img-circle", function() {
+            $("div#alex").hide("slow");
+        });
+
+        $(".avatar").on("click", ".img-circle", function() {
+            $("#avatarka").trigger("click");
+        });
     }
 }
-
-$('.image-list').on('click', '.remove-photo', function(event) {
-    event.preventDefault();
-    var image = $(this).parent().removeClass('fancybox');
-    var imageID = {id: $(this).attr('value')};
-    console.log(imageID);
-    $.post('/remove_photo', imageID, function(res) {
-        if (res.status) {
-            image.remove();
-        }
-    });
-});
-
-if(guest==false) {
-$(".avatar").on("mouseover", ".img-circle", function() {
-    $("div#alex").show("slow");
-}).on("mouseout", ".img-circle", function() {
-    $("div#alex").hide("slow");
-});
-
-$(".avatar").on("click", ".img-circle", function() {
-    $("#avatarka").trigger("click");
-});
-};
-
-};
 
 $(document).ready(function() {
     userPage();
     $(".fancybox").fancybox({
-        openEffect  : 'fade',
-        openSpeed : 'slow',
-        closeEffect : 'fade',
-        closeSpeed : 'slow' 
+        openEffect: 'fade',
+        openSpeed: 'slow',
+        closeEffect: 'fade',
+        closeSpeed: 'slow'
     });
 });
